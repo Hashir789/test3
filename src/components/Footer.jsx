c74d786d33735df4630b533c3ba010ad7118f54e
Hashir789 <m.hashir.malik1020@gmail.com>
2024-03-02 13:22:18 +0500

    Commit 5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,52 +9,52 @@ const Footer = () => {
             <Grid container>
                 <Grid item xs={12} sm={5} sx={{ borderRight: { sm: '1px solid rgba(0,0,0,0.1)', xs: 'none' } }} px={3}>
                     <Box>
-                        <Typography sx={{ fontFamily: "'Lateef', serif", fontSize: { sm: '2.75vw', xs: '8vw' }, color: 'white', fontWeight: 'bold' }}>Gennotate</Typography>
-                        <Typography sx={{ fontSize: { sm: '1.2vw', xs: '3vw' }, color: 'white', lineHeight: 1.2 }} textAlign='justify'>Gennotate, meticulously developed by NUST students, serves as a comprehensive web application dedicated to the precise generation and segmentation of Optical Coherence Tomography (OCT) images, reflecting a seamless blend of simplicity and functionality.</Typography>
-                        <Box sx={{ color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }} mt={{ xs: 0, sm: 1 }} mb={{ xs: 2, sm: 0 }}>
-                            <Tooltip title="Github" arrow>
-                                <IconButton><FaGithub style={{ color: 'white', fontSize: { xs: '1.2vw', sm: '3vw' } }} /></IconButton>
+                        <Typography sx={{ fontFamily: "'Lateef', serif", fontSize: { sm: '2.75vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Gennotate</Typography>
+                        <Typography sx={{ fontSize: { sm: '1.2vw', xs: '4vw' }, color: 'white', lineHeight: 1.2 }} textAlign='justify'>Gennotate, meticulously developed by NUST students, serves as a comprehensive web application dedicated to the precise generation and segmentation of Optical Coherence Tomography (OCT) images, reflecting a seamless blend of simplicity and functionality.</Typography>
+                        <Box sx={{ color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }} mt={{ xs: 1, sm: 1 }} mb={{ xs: 1, sm: 0 }}>
+                            <Tooltip title="Github">
+                                <IconButton><FaGithub style={{ color: 'white', fontSize: { xs: '1.2vw', sm: '4vw' } }} /></IconButton>
                             </Tooltip>
-                            <Tooltip title="LinkedIn" arrow>
-                                <IconButton><FaLinkedin style={{ color: 'white', fontSize: { xs: '1.2vw', sm: '3vw' } }}/></IconButton>
+                            <Tooltip title="LinkedIn">
+                                <IconButton><FaLinkedin style={{ color: 'white', fontSize: { xs: '1.2vw', sm: '4vw' } }}/></IconButton>
                             </Tooltip>
-                            <Tooltip title="Twitter" arrow>
-                                <IconButton><FaTwitter style={{ color: 'white', fontSize: { xs: '1.2vw', sm: '3vw' } }}/></IconButton>
+                            <Tooltip title="Twitter">
+                                <IconButton><FaTwitter style={{ color: 'white', fontSize: { xs: '1.2vw', sm: '4vw' } }}/></IconButton>
                             </Tooltip>
                         </Box>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={2} sx={{ borderRight: { sm: '1px solid rgba(0,0,0,0.1)', xs: 'none' } }} px={3}>
                     <Box py={{ xs: 1, sm: 2 }}>
-                        <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: '#154d4f', fontWeight: 'bold' }}>Services</Typography>
+                        <Typography sx={{ fontSize: { xs: '5vw', sm: '1.2vw' }, color: '#154d4f', fontWeight: 'bold' }}>Services</Typography>
                         <Box py={{ xs: 1, sm: 2 }}>
-                            <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>Generation</Typography>
-                            <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>Segmentation</Typography>
+                            <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>Generation</Typography>
+                            <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>Segmentation</Typography>
                         </Box>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={2} sx={{ borderRight: { sm: '1px solid rgba(0,0,0,0.1)', xs: 'none' } }} px={3}>
                     <Box py={{ xs: 1, sm: 2 }}>
-                        <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: '#154d4f', fontWeight: 'bold' }}>Pages</Typography>
+                        <Typography sx={{ fontSize: { xs: '5vw', sm: '1.2vw' }, color: '#154d4f', fontWeight: 'bold' }}>Pages</Typography>
                         <Box py={{ xs: 1, sm: 2 }}>
-                            <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>Home</Typography>
-                            <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>Gallery</Typography>
-                            <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>Generate</Typography>
-                            <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>Our Team</Typography>
+                            <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>Home</Typography>
+                            <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>Gallery</Typography>
+                            <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>Generate</Typography>
+                            <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>Our Team</Typography>
                         </Box>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={3} px={3}>
                     <Box py={{ xs: 1, sm: 2 }}>
-                        <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: '#154d4f', fontWeight: 'bold' }}>Contact Us</Typography>
+                        <Typography sx={{ fontSize: { xs: '5vw', sm: '1.2vw' }, color: '#154d4f', fontWeight: 'bold' }}>Contact Us</Typography>
                         <Box py={{ xs: 1, sm: 2 }}>
                             <Box sx={{ display: 'flex' }}>
-                                <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white', fontWeight: 'bold' }}>Email:&nbsp;</Typography>
-                                <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>gennotate@gmail.com</Typography>
+                                <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white', fontWeight: 'bold' }}>Email:&nbsp;</Typography>
+                                <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>gennotate@gmail.com</Typography>
                             </Box>
                             <Box sx={{ display: 'flex' }}>
-                                <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white', fontWeight: 'bold' }}>Phone:&nbsp;</Typography>
-                                <Typography sx={{ fontSize: { xs: '3vw', sm: '1.2vw' }, color: 'white' }}>+92 321 2165297</Typography>
+                                <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white', fontWeight: 'bold' }}>Phone:&nbsp;</Typography>
+                                <Typography sx={{ fontSize: { xs: '4vw', sm: '1.2vw' }, color: 'white' }}>+92 321 2165297</Typography>
                             </Box>
                         </Box>
                     </Box>
@@ -62,7 +62,7 @@ const Footer = () => {
             </Grid>
         </Box>
         <Box mt={2}>
-            <Typography textAlign='center' sx={{ color: 'white', fontSize: { sm: '1.2vw', xs: '3vw' } }}>© All rights reserved · Gennotate · Privacy Policy</Typography>
+            <Typography textAlign='center' sx={{ color: 'white', fontSize: { sm: '1.2vw', xs: '4vw' } }}>© All rights reserved · Gennotate · Privacy Policy</Typography>
         </Box>
     </Box>
   )

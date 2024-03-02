ab31acd7656518f50b847053529aea415d41462e
Hashir789 <m.hashir.malik1020@gmail.com>
2024-03-02 23:14:14 +0500

    Anythingg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import { FaCircleUser } from "react-icons/fa6";
-import { AppBar, Box, Typography } from '@mui/material'
+import { FaCircleUser, FaBars, FaXmark } from "react-icons/fa6";
+import { AppBar, Box, Typography, Checkbox } from '@mui/material'
 
 const Navbar = () => {
   return (
     <Box>
-      <AppBar style={{ background: 'linear-gradient(to bottom, #0ea190, #11b97c)', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-        <Typography sx={{ fontFamily: "'Lateef', serif", fontSize: { sm: '3vw', xs: '12vw' }, color: '#154d4f', fontWeight: 'bold' }} pl={6}>Gennotate</Typography>
-        <Box sx={{ display: { sm: 'flex', xs: 'none' }, width: '30vw', alignItems: 'center', justifyContent: 'space-between' }}>
-          <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Home</Typography>
-          <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Gallery</Typography>
-          <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Generate</Typography>
-          <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Our Team</Typography>
+      <AppBar style={{ background: 'linear-gradient(to bottom, #0ea190, #11b97c)' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Typography sx={{ fontFamily: "'Lateef', serif", fontSize: { sm: '3vw', xs: '12vw' }, color: '#154d4f', fontWeight: 'bold' }} pl={6}>Gennotate</Typography>
+          <Box sx={{ display: { sm: 'flex', xs: 'none' }, width: '30vw', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Home</Typography>
+            <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Gallery</Typography>
+            <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Generate</Typography>
+            <Typography sx={{ fontSize: { sm: '1.2vw', xs: '12vw' }, color: 'white', fontWeight: 'bold' }}>Our Team</Typography>
+          </Box>
+          <Box mr={6}sx={{ display: {xs: 'none', sm: 'flex'} }}><FaCircleUser style={{  color: '#154d4f', background: 'inherit' }} size={30}/></Box>
+          <Box mr={6}sx={{ display: {xs: 'flex', sm: 'none'} }}>
+            <Checkbox icon={<FaBars style={{  color: '#154d4f', background: 'inherit' }} size={26}/>} checkedIcon={<FaXmark style={{  color: '#154d4f', background: 'inherit' }} size={26}/>} />  
+          </Box>
         </Box>
-        <Box mr={6}sx={{ display: 'flex' }}><FaCircleUser style={{  color: '#154d4f', background: 'inherit' }} size={24}/></Box>
+        {/* <Box px={6} pb={2}>
+          <Box my={1}><Typography sx={{ fontSize: { sm: '1.2vw', xs: '6vw' }, color: 'white', fontWeight: 'bold' }}>Home</Typography></Box>
+          <Box my={1}><Typography sx={{ fontSize: { sm: '1.2vw', xs: '6vw' }, color: 'white', fontWeight: 'bold' }}>Gallery</Typography></Box>
+          <Box my={1}><Typography sx={{ fontSize: { sm: '1.2vw', xs: '6vw' }, color: 'white', fontWeight: 'bold' }}>Generate</Typography></Box>
+          <Box my={1}><Typography sx={{ fontSize: { sm: '1.2vw', xs: '6vw' }, color: 'white', fontWeight: 'bold' }}>Our Team</Typography></Box>
+        </Box> */}
       </AppBar>
     </Box>
   )

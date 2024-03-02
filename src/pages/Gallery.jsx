e36fd583c75d00da5097e88ce387b6e4cae152dc
Hashir789 <m.hashir.malik1020@gmail.com>
2024-03-02 20:41:39 +0500

    Nothing

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,9 +2,11 @@ import { Box, Button } from '@mui/material'
 import React from 'react'
 import Footer from '../components/Footer'
 import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 const Gallery = () => {
   return (
     <Box>
+      <Navbar/>
       <Box sx={{ height: '90vh' }}></Box>
       <Footer/>
       <Link to='/gallery'><Button>Gallery</Button></Link>
